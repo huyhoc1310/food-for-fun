d813faa3bc7667d50bac749a155f1e5b73efb64a
Nguyen Duc Huy Hoc <huyhoc1310@gmail.com>
2018-08-06 16:58:08 +0700

    Build static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    base_title = "Food for fun"
+    if page_title.empty?
+      base_title
+    else page_title + " | " + base_title
+    end
+  end
 end
--- app/models/suggest.rb
+++ app/models/suggest.rb
@@ -1,4 +1,4 @@
 class Suggest < ApplicationRecord
-  belongs_to :user,foreign_key: :user_id
+  belongs_to :user, foreign_key: :user_id
   belongs_to :restaurant, foreign_key: :restaurant_id
 end
