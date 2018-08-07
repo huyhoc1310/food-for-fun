722309d28af1d4686a0a905a913dd5e45ce39bc2
hang <hangtran9397@gmail.com>
2018-08-07 11:31:40 +0700

    Manager update menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   include OrderDetailsHelper
   include RestaurantsHelper
   include FoodsHelper
+  include CategoriesHelper
 
   private
 
---
+++ app/controllers/manager/menus_controller.rb
@@ -0,0 +1,36 @@
+class Manager::MenusController < ApplicationController
+  before_action :find_restaurant, only: [:show]
+  before_action :find_food, only: [:edit]
+
+  def show
+    @foods = Food.find_food_enable(params[:id]).page(params[:page]).per Settings.rows
+  end
+
+  def edit; end
+
+  def delete_food
+    @food = Food.find_by_id params[:menu_id]
+    restaurant_id = @food.restaurant_id
+    if @food.update_attributes status: :disable
+      flash[:success] = t "food.message.disable_success"
+      redirect_to manager_menu_path(restaurant_id)
+    else
+      flash[:danger] = t "food.message.disable_fail"
+      redirect_to manager_menu_path(restaurant_id)
+    end
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find_by_id params[:id]
+    return if @restaurant.nil?
+  end
+
+  def find_food
+    @food = Food.find_by_id params[:id]
+    @categories = Category.all
+    restaurant_id = @food.restaurant_id
+    return if @food.nil?
+  end
+end
