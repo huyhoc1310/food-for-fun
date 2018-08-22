ef195b2d97d1625325ec7023ae91e8d2b126ccd4
Huy Hoc <huyhoc1310@gmail.com>
2018-08-22 09:05:57 +0700

    itegrate google maps

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_19_033805) do
+ActiveRecord::Schema.define(version: 2018_08_22_090933) do
 
   create_table "average_caches", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "rater_id"
@@ -148,6 +148,8 @@ ActiveRecord::Schema.define(version: 2018_08_19_033805) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
