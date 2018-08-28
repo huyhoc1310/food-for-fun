e204b92b7acb2b3c5f68b1da6d6a415fa7882d03
Huy Hoc <huyhoc1310@gmail.com>
2018-08-28 09:56:26 +0700

    deploy heroku

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,11 +4,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [Settings.size_img, Settings.size_img]
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  #if Rails.env.production?
+   # storage :fog
+  #else
+  storage :file
+  #end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.create!(name: "user1",
-             email: "demo@gmail.com",
+             email: "admin@gmail.com",
              address: "141 xuan thuy, cau giay, ha noi",
              phone_number: "02548652",
              role: 2,
@@ -9,7 +9,7 @@ User.create!(name: "user1",
              activated_at: Time.zone.now)
 
 User.create!(name: "user2",
-             email: "demo1@gmail.com",
+             email: "manager@gmail.com",
              address: "141 xuan thuy, cau giay, ha noi",
              phone_number: "02548652",
              role: 1,
@@ -18,21 +18,15 @@ User.create!(name: "user2",
              activated: true,
              activated_at: Time.zone.now)
 
-5.times do |n|
-  name = Faker::Name.name
-  email = "demo-#{n+2}@gmail.com"
-  address = "Ha Noi"
-  phone_number = "016548522"
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               address: address,
-               phone_number: phone_number,
-               password: password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
-end
+User.create!(name: "user3",
+             email: "user@gmail.com",
+             address: "141 xuan thuy, cau giay, ha noi",
+             phone_number: "02548652",
+             role: 0,
+             password: "123456",
+             password_confirmation: "123456",
+             activated: true,
+             activated_at: Time.zone.now)
 
 Restaurant.create!(name: "Restaurant A",
              description: "this is the great restaurant.",
